9dcf597bba06e42961cb8389542de230d31207ff
alxjnr <alexrobertjoiner@gmail.com>
2023-03-19 14:56:33 +0000

    add virtual keyboard to home

--- src/components/VirtualKeyboard.jsx
+++ src/components/VirtualKeyboard.jsx
@@ -56,42 +56,302 @@ const VirtualKeyboard = ({ setInput }) => {
         >
           6
         </button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button>0</button>
+        <button
+          onClick={() => {
+            setInput((input) => {
+              return input + "7";
+            });
+          }}
+        >
+          7
+        </button>
+        <button
+          onClick={() => {
+            setInput((input) => {
+              return input + "8";
+            });
+          }}
+        >
+          8
+        </button>
+        <button
+          onClick={() => {
+            setInput((input) => {
+              return input + "9";
+            });
+          }}
+        >
+          9
+        </button>
+        <button
+          onClick={() => {
+            setInput((input) => {
+              return input + "0";
+            });
+          }}
+        >
+          0
+        </button>
+        <button
+          onClick={() => {
+            setInput((input) => {
+              return input.slice(0, input.length - 1);
+            });
+          }}
+        >
+          {"<"}
+        </button>
       </section>
       <section className="virtual-keyboard-row">
-        <button>Q</button>
-        <button>W</button>
-        <button>E</button>
-        <button>R</button>
-        <button>T</button>
-        <button>Y</button>
-        <button>U</button>
-        <button>I</button>
-        <button>O</button>
-        <button>P</button>
+        <button
+          onClick={() => {
+            setInput((input) => {
+              return input + "Q";
+            });
+          }}
+        >
+          Q
+        </button>
+        <button
+          onClick={() => {
+            setInput((input) => {
+              return input + "W";
+            });
+          }}
+        >
+          W
+        </button>
+        <button
+          onClick={() => {
+            setInput((input) => {
+              return input + "E";
+            });
+          }}
+        >
+          E
+        </button>
+        <button
+          onClick={() => {
+            setInput((input) => {
+              return input + "R";
+            });
+          }}
+        >
+          R
+        </button>
+        <button
+          onClick={() => {
+            setInput((input) => {
+              return input + "T";
+            });
+          }}
+        >
+          T
+        </button>
+        <button
+          onClick={() => {
+            setInput((input) => {
+              return input + "Y";
+            });
+          }}
+        >
+          Y
+        </button>
+        <button
+          onClick={() => {
+            setInput((input) => {
+              return input + "U";
+            });
+          }}
+        >
+          U
+        </button>
+        <button
+          onClick={() => {
+            setInput((input) => {
+              return input + "I";
+            });
+          }}
+        >
+          I
+        </button>
+        <button
+          onClick={() => {
+            setInput((input) => {
+              return input + "O";
+            });
+          }}
+        >
+          O
+        </button>
+        <button
+          onClick={() => {
+            setInput((input) => {
+              return input + "P";
+            });
+          }}
+        >
+          P
+        </button>
       </section>
       <section className="virtual-keyboard-row">
-        <button>A</button>
-        <button>S</button>
-        <button>D</button>
-        <button>F</button>
-        <button>G</button>
-        <button>H</button>
-        <button>J</button>
-        <button>K</button>
-        <button>L</button>
+        <button
+          onClick={() => {
+            setInput((input) => {
+              return input + "A";
+            });
+          }}
+        >
+          A
+        </button>
+        <button
+          onClick={() => {
+            setInput((input) => {
+              return input + "S";
+            });
+          }}
+        >
+          S
+        </button>
+        <button
+          onClick={() => {
+            setInput((input) => {
+              return input + "D";
+            });
+          }}
+        >
+          D
+        </button>
+        <button
+          onClick={() => {
+            setInput((input) => {
+              return input + "F";
+            });
+          }}
+        >
+          F
+        </button>
+        <button
+          onClick={() => {
+            setInput((input) => {
+              return input + "G";
+            });
+          }}
+        >
+          G
+        </button>
+        <button
+          onClick={() => {
+            setInput((input) => {
+              return input + "H";
+            });
+          }}
+        >
+          H
+        </button>
+        <button
+          onClick={() => {
+            setInput((input) => {
+              return input + "J";
+            });
+          }}
+        >
+          J
+        </button>
+        <button
+          onClick={() => {
+            setInput((input) => {
+              return input + "K";
+            });
+          }}
+        >
+          K
+        </button>
+        <button
+          onClick={() => {
+            setInput((input) => {
+              return input + "L";
+            });
+          }}
+        >
+          L
+        </button>
       </section>
       <section className="virtual-keyboard-row">
-        <button>Z</button>
-        <button>X</button>
-        <button>C</button>
-        <button>V</button>
-        <button>B</button>
-        <button>N</button>
-        <button>M</button>
+        <button
+          onClick={() => {
+            setInput((input) => {
+              return input + "Z";
+            });
+          }}
+        >
+          Z
+        </button>
+        <button
+          onClick={() => {
+            setInput((input) => {
+              return input + "X";
+            });
+          }}
+        >
+          X
+        </button>
+        <button
+          onClick={() => {
+            setInput((input) => {
+              return input + "C";
+            });
+          }}
+        >
+          C
+        </button>
+        <button
+          onClick={() => {
+            setInput((input) => {
+              return input + "V";
+            });
+          }}
+        >
+          V
+        </button>
+        <button
+          onClick={() => {
+            setInput((input) => {
+              return input + "B";
+            });
+          }}
+        >
+          B
+        </button>
+        <button
+          onClick={() => {
+            setInput((input) => {
+              return input + "N";
+            });
+          }}
+        >
+          N
+        </button>
+        <button
+          onClick={() => {
+            setInput((input) => {
+              return input + "M";
+            });
+          }}
+        >
+          M
+        </button>
+      </section>
+      <section className="virtual-keyboard-row-space">
+        <button
+          onClick={() => {
+            setInput((input) => {
+              return input + " ";
+            });
+          }}
+        >
+          SPACE
+        </button>
       </section>
     </section>
   );
